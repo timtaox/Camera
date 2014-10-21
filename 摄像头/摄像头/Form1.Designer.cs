5d315a14e008442a21acaeec311431051e68b257
JonyTao <timtaox@163.com>
2014-10-21 16:52:29 +0800

    C# call windows camera
    C# call windows camera

---
+++ 摄像头/摄像头/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace 摄像头
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.btn_Capture = new System.Windows.Forms.Button();
+            this.btn_Video = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(860, 545);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(108, 13);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(75, 23);
+            this.btn_Open.TabIndex = 1;
+            this.btn_Open.Text = "打开摄像头";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(236, 13);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 23);
+            this.btn_Close.TabIndex = 1;
+            this.btn_Close.Text = "关闭摄像头";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // btn_Capture
+            // 
+            this.btn_Capture.Location = new System.Drawing.Point(364, 12);
+            this.btn_Capture.Name = "btn_Capture";
+            this.btn_Capture.Size = new System.Drawing.Size(75, 23);
+            this.btn_Capture.TabIndex = 2;
+            this.btn_Capture.Text = "抓图";
+            this.btn_Capture.UseVisualStyleBackColor = true;
+            this.btn_Capture.Click += new System.EventHandler(this.btn_Capture_Click);
+            // 
+            // btn_Video
+            // 
+            this.btn_Video.Location = new System.Drawing.Point(491, 13);
+            this.btn_Video.Name = "btn_Video";
+            this.btn_Video.Size = new System.Drawing.Size(75, 23);
+            this.btn_Video.TabIndex = 2;
+            this.btn_Video.Text = "录像";
+            this.btn_Video.UseVisualStyleBackColor = true;
+            this.btn_Video.Click += new System.EventHandler(this.btn_Video_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 610);
+            this.Controls.Add(this.btn_Video);
+            this.Controls.Add(this.btn_Capture);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Capture;
+        private System.Windows.Forms.Button btn_Video;
+    }
+}
+
---
+++ 摄像头/摄像头/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace 摄像头
+{
+    public partial class Form1 : Form
+    {
+        Pick pick;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int left = 0;
+            int top = 0;
+            int width = 352;
+            int w = pictureBox1.Width;
+            int height = 288;
+            int h = pictureBox1.Height;
+            pick = new Pick(pictureBox1.Handle, left, top, w, h);
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            pick.Stop();
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            pick.Start();
+        }
+
+        private void btn_Capture_Click(object sender, EventArgs e)
+        {
+            pick.GrabImage("D:\\测试.jpg");
+        }
+
+        private void btn_Video_Click(object sender, EventArgs e)
+        {
+            if (btn_Video.Text == "录像")
+            {
+                pick.Kinescope("D:\\测试.avi");
+                btn_Video.Text = "停止录像";
+            }
+            else
+            {
+                pick.StopKinescope();
+                btn_Video.Text = "录像";
+            }
+        }
+    }
+}
